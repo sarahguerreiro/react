f24555c71cdd6fc892518fa79c6f6892121b3a40
Sarah <sdmguerre@gmail.com>
2024-02-14 20:11:04 -0300

    refrescando o conteudo de import e export em js

--- js_refresh/app.js
+++ js_refresh/app.js
@@ -1,5 +1,17 @@
-/*Uma vez que vc tem o arquivo desejado com a palavra-chave export na frente, é possível visualizar este arquivo a partir de outro arquivo, adicionando ao arquivo que chama a palavra-chave import, seguindo esse padrão: */
+/*Uma vez que vc tem o arquivo desejado com a palavra-chave export na frente, é possível visualizar este arquivo a partir de outro arquivo, adicionando ao arquivo que chama a palavra-chave import, seguindo esse padrão: 
 
 import {apiKey} from "./main.js"
 
 console.log(apiKey)
+*/
+
+/*mas se a sua escolha de sintaxe de importação for a padrão, vc não precisa usar o nome entre chaves e tbm nao precisa dar o mesmo nome qe a coisa a ser exporta, ate pq se vc reparar bem, na exportação padrao vc não indica o que vai ser exportado pelo nome, e sim pelo valor, mas vc deve colocar um nome. Apesar dessa possibilidade, obviamente é indicado que vc atribua aqui o mesmo nome daquilo que vc quer exportar e tbm o caminho deste arquivo: */
+
+// import apiKey from "./main.js"
+
+// console.log(apiKey)
+
+
+/*
+essa maneira de importar é especialmente util quando o que vc tem a importar é apenas uma variavel/uma função, um unico valor no arquivo para o qual deseja exportar
+*/
--- js_refresh/main.js
+++ js_refresh/main.js
@@ -1,13 +1,23 @@
 /* Sintaxe de importação e exportação:
 é considerado uma pratica recomendada dividir seu codigo em varios arquivos afim de mante-lo sustentavel e gerenciavel, e é exatamete o que vc faz com a ajuda das palavras-chave de importação e exportação.
-*/
 
-/*no exemplo a seguir, temos uma string com uma chave de api x que talvez seja necessaria p enviar solicitações http a um back-end:
+no exemplo a seguir, temos uma string com uma chave de api x que talvez seja necessaria p enviar solicitações http a um back-end:/*
 
-let apiKey = 'basd64bajs5hasdb16'
+
+let apiKey = 'basd64bajs5hasdb16';
 
 Para que essa chave de api, esse valor dessa string esteja disponivel em outros arquivos, vc precisa exportar essa string, adicionando a palavra-chave export na frente dela: */
 
-export let apiKey = 'basd64bajs5hasdb16'
+//export let apiKey = 'basd64bajs5hasdb16';
 
-/*com essa palavra magica, essa variavel fica disponivel e visivel fora deste arquivo*/
+/*com essa palavra magica, essa variavel fica disponivel e visivel fora deste arquivo
+usando o export sem o default, vc pode exportar quantascoisas quiser, ou seja, pode ter mais de um export num mesmo arquivo.
+
+vc pode ainda criar a chamada exportação padrão, adicionando a palavra-chave default apos a palavra reservada export seguido do valor da função/variavel/class/wtv que vc quiser exportar:
+*/
+
+export default 'basd64bajs5hasdb16';
+
+/*
+ao adicionar a palavra chave default, vc esta dizendo q o valor passado aqui deve ser o padrão exportado por esse arquivo. Se vc optar pela exportação padrão, lembre-se que vc so pode ter uma exportação padrão/arquivo.
+*/
